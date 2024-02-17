a48d147dcf87bae81459adf1dcb7622fbcade295
harithasaraswathyrk <harithasaraswathy01@gmail.com>
2024-02-17 11:54:59 +0530

    profile

--- script.js
+++ script.js
@@ -117,6 +117,14 @@ document.addEventListener('DOMContentLoaded', function()
   }
 });
 */
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.getElementById('menu-icon');
+  const navList = document.querySelector('.navlist');
+
+  menuIcon.addEventListener('click', function () {
+      navList.classList.toggle('active');
+  });
+});
 
 /*-------------------observer------------------------------*/
 
