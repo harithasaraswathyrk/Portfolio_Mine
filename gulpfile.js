984c750c867d8a9b6cb70f7ef532967292722357
harithasaraswathyrk <harithasaraswathy01@gmail.com>
2024-02-17 09:50:38 +0530

    profile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+// Define a task to process JavaScript files
+function jsTask() {
+    return src('src/js/*.js') // Path to your JavaScript files
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('script.js')) // Concatenate all files into one
+        .pipe(uglify()) // Minify the JavaScript code
+        .pipe(dest('dist/js')); // Output directory
+}
+
+// Export the task to make it available in the Gulpfile
+exports.jsTask = jsTask;
